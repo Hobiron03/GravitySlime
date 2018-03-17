73d033971ecec96134d714bfd3e1f5e2c7ecd29f
Hobiron <yudainepnep@icloud.com>
2018-03-18 01:34:15 +0900

    微調整

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,36 +5,42 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     Vector2 acc_vec;
+    Vector3 gravityPosY = new Vector3(0, -9.7f, 0);
+    Vector3 gravityNegY = new Vector3(0, 9.7f, 0);
+
     float power = 30.0f;
+    float maxSpeed = 30.0f;
     Rigidbody2D rgbody;
 	// Use this for initialization
 	void Start () 
     {
         rgbody = this.GetComponent<Rigidbody2D>();
-       
+
 	}
 	
 	// Update is called once per frame
-	/*void Update () 
+	void Update () 
     {
-
-        acc_vec.x = Input.acceleration.x;
-
-        acc_vec.y = Input.acceleration.y;
-        Debug.Log(acc_vec.y);
-
-        if (acc_vec != null)
-        {
-            Physics.gravity = new Vector3(acc_vec.x*3, -acc_vec.y*3, 0);
-        }
+        
 		
-	}*/
+	}
 
     void FixedUpdate()
     {
         // 加速度与える
         acc_vec.x = Input.acceleration.x;
         acc_vec.y = Input.acceleration.y;
+
+        if(acc_vec.y < 0f)
+        {
+            Physics.gravity = gravityPosY;
+        }
+        else
+        {
+            Physics.gravity = gravityNegY; 
+        }
+
+        Debug.Log(Physics.gravity);
 
         rgbody.AddForce(acc_vec*power, ForceMode2D.Force);
     }
