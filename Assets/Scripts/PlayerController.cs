a51ed894efc0bc89d622320bb54c370769bb8b38
川上雄大 <yudainepnep@icloud.com>
2018-03-25 22:50:34 +0900

    微調整

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,9 +7,11 @@ public class PlayerController : MonoBehaviour {
     private Vector2 acc_vec;
     private Vector3 gravUpY = new Vector3(0, -9.7f, 0);
     private Vector3 gravDownY = new Vector3(0, 9.7f, 0);
+    private Vector3 gravDownX = new Vector3(-9.7f, 0, 0);
+    private Vector3 gravUpX = new Vector3(9.7f, 0, 0);
 
-    private float power = 15.0f;
-    private float maxSpeed = 7.0f;
+    private float power = 13.0f;
+    private float maxSpeed = 6.0f;
 
     private Rigidbody2D rgbody;
     private AudioSource audio;
@@ -38,13 +40,25 @@ public class PlayerController : MonoBehaviour {
         acc_vec.x = Input.acceleration.x;
         acc_vec.y = Input.acceleration.y;
 
-        if(acc_vec.y < 0f)
-        {
-            Physics.gravity = gravUpY;
-        }
-        else
+
+        if(-0.7f < acc_vec.y && acc_vec.y < -1.2f)
         {
             Physics.gravity = gravDownY; 
+            //Debug.Log(Physics.gravity);
+        }
+        else if (0.7f < acc_vec.y && acc_vec.y < 1.2f)
+        {
+            Physics.gravity = gravUpY;
+            //Debug.Log(Physics.gravity);
+        }
+        else if(-0.7f < acc_vec.x && acc_vec.x < -1.2f)
+        {
+            Physics.gravity = gravDownX;
+        }
+        else if (0.7f < acc_vec.x && acc_vec.x < 1.2f)
+        {
+            Physics.gravity = gravUpX;
+            //Debug.Log(Physics.gravity);
         }
 
         float speedX = Mathf.Abs(rgbody.velocity.x);
